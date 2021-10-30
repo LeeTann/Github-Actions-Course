ad972238f3a6b1617ef2921f77c9443fe56c7171
Lee Tann <leeroyleeroy18@gmail.com>
2021-10-30 11:02:35 -0700

    custom issue action

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+    // This should be a token with access to your repository scoped in as a secret.
+    // The YML workflow will need to set myToken with the GitHub Secret Token
+    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
+    const myToken = core.getInput('token');
+
+    const octokit = github.getOctokit(myToken)
+
+    // You can also pass in additional options as a second parameter to getOctokit
+    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+
+    const response = await octokit.rest.pulls.get({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        title,
+        body,
+        assignees: assignees ? assignees.split('\n') : undefined
+    });
+
+    console.log(response.data);
+    core.setOutput('issue', JSON.stringify(response.data))
+}
+
+run();
